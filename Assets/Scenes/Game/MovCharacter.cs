aa15956620f1aee2010787d307eedb7b1574e05e
codingworks2022 <98422397+LSG124@users.noreply.github.com>
2023-06-29 20:24:01 -0400

    Movimiento Personaje
    Script para mover al personaje con teclas WASD

---
+++ Assets/Scenes/Game/MovCharacter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovCharacter : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public Rigidbody2D rb;
+
+    public void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.velocity = movement * speed;
+    }
+}
